6f619800a6d01023e3b51b4c8ebd4be8e0c7a132
bobanDev <266649156+jethdev@users.noreply.github.com>
2026-05-05 00:44:42 +0200

    add base

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App = () => (
         <TooltipProvider>
           <Toaster />
           <Sonner />
-          <BrowserRouter>
+          <BrowserRouter basename="/vrtni-drug-pomoc">
             <Routes>
               <Route path="/" element={<Index />} />
               {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
