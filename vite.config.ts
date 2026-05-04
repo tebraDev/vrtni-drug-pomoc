3ec0d55aadf6074476ca36a9e54f457d95c355df
tebra <36519499+tebraDev@users.noreply.github.com>
2026-05-04 08:14:48 +0000

    base

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/",
+  base: "/vrtni-drug-pomoc/",
   server: {
     host: "::",
     port: 8080,
