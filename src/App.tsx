08a5e86bdf06dfa710886989119870379f79645d
tebra <36519499+tebraDev@users.noreply.github.com>
2026-05-04 08:16:07 +0000

    add basename

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <BrowserRouter basename="/vrtni-drug-pomoc">
           <Routes>
             <Route path="/" element={<Index />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
